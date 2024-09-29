84acf31c7530d1472f852900eeae71d64b6aa012
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2024-09-29 12:16:45 +0330

    re-disign footer

--- partials/parts/footer/template.php
+++ partials/parts/footer/template.php
@@ -1,8 +1,12 @@
-<footer class="mt-6 bg-no-repeat bg-[left_16px_top_0] max-md:!bg-none after:bg-black/35 after:content-[''] after:inset-0 after:absolute relative isolate after:-z-10"
-		style="background-image: url('<?php echo get_option( 'footer_bg_image' ) ?>');">
+<footer
+		class="mt-6 max-md:!bg-none after:bg-black/35 after:content-[''] after:inset-0 after:absolute relative isolate after:-z-10">
+	<div class="absolute h-4 top-0 w-full bg-contain"
+		 style="background-image: url('<?php echo get_option( 'footer_bg_image' ) ?>');"> </div>
 	<div class="py-11 px-5 grid gap-10">
 		<?php cyn_get_part( '/footer/items' ) ?>
 
 		<?php cyn_get_part( '/footer/social-media' ) ?>
 	</div>
+	<div class="absolute h-4 bottom-0 w-full bg-contain"
+		 style="background-image: url('<?php echo get_option( 'footer_bg_image' ) ?>');"> </div>
 </footer>
